96350409c2b944b0a4b3af7c14e831a8c2519aaf
jdmasciano2 <jdmasciano2@gmail.com>
2024-10-18 12:43:55 +0000

    reown connect button

--- frontend/seed/app/page.tsx
+++ frontend/seed/app/page.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { BackgroundLines } from "@/components/ui/background-lines";
+import { Navbar } from "@/components/ui/nav";
 
 export default function BackgroundLinesDemo() {
   return (
+    <>
+    <Navbar className="top-2" />
+  
     <BackgroundLines className="flex items-center justify-center w-full flex-col px-4 bg-black">  {/* Add bg-black for the background */}
       <h2 className="bg-clip-text text-white text-center text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
         Multiseed
@@ -11,5 +15,7 @@ export default function BackgroundLinesDemo() {
       MultiSig Wallet for Decentralized Fund Management
       </p>
     </BackgroundLines>
+    </>
+    
   );
 }
