b6da489fa98acd4103d9aa5eff01613520c89700
jdmasciano2 <jdmasciano2@gmail.com>
2024-10-18 13:58:40 +0000

    disable eslint

--- frontend/seed/components/ui/navbar-menu.tsx
+++ frontend/seed/components/ui/navbar-menu.tsx
@@ -109,6 +109,7 @@ export const ProductItem = ({
   );
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const HoveredLink = ({ children, ...rest }: any) => {
   return (
     <Link
